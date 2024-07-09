821dd97cd22c9c0d302b28b1ab5720106b6ff52e
Nolan Pollack <nolanfpollack@gmail.com>
2024-07-09 16:31:06 -0400

    fixed errors and reformatted with Result conventions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
+use std::num::ParseIntError;
+
 use colored::Colorize;
 
 const BOARD_SIZE: usize = 8;
+
+#[derive(Clone, Copy, PartialEq)]
+struct Coordinate {
+    x: usize,
+    y: usize,
+}
+
+impl Coordinate {
+    fn new(x: usize, y: usize) -> Coordinate {
+        if x >= BOARD_SIZE || y >= BOARD_SIZE {
+            panic!("Coordinate must be within the bounds of the board");
+        }
+        Coordinate { x, y }
+    }
+}
 
 #[derive(Clone, Copy)]
 enum ChessPiece {
     Black(PieceType),
     White(PieceType),
+}
+
+impl ChessPiece {
+    fn can_take(&self, other: ChessPiece, from: Coordinate, to: Coordinate) -> bool {
+        match self {
+            ChessPiece::Black(t) => match other {
+                ChessPiece::Black(_) => false,
+                ChessPiece::White(_) => t.is_move_valid(from, to),
+            },
+            ChessPiece::White(t) => match other {
+                ChessPiece::Black(_) => t.is_move_valid(from, to),
+                ChessPiece::White(_) => false,
+            },
+        }
+    }
 }
 
 #[derive(Clone, Copy)]
@@ -30,32 +62,35 @@ impl PieceType {
         }
     }
 
-    fn is_move_valid(&self, from: (usize, usize), to: (usize, usize)) -> bool {
+    // Assumes that the piece is not moving to its own spot
+    fn is_move_valid(&self, from: Coordinate, to: Coordinate) -> bool {
+        let x_move = from.x as i32 - to.x as i32;
+        let y_move = from.y as i32 - to.y as i32;
         match self {
             PieceType::King => {
-                let dx = (from.0 as i32 - to.0 as i32).abs();
-                let dy = (from.1 as i32 - to.1 as i32).abs();
+                let dx = x_move.abs();
+                let dy = y_move.abs();
                 dx <= 1 && dy <= 1
             }
             PieceType::Queen => {
-                let dx = (from.0 as i32 - to.0 as i32).abs();
-                let dy = (from.1 as i32 - to.1 as i32).abs();
-                dx == dy || from.0 == to.0 || from.1 == to.1
+                let dx = x_move.abs();
+                let dy = y_move.abs();
+                dx == dy || from.x == to.x || from.y == to.y
             }
-            PieceType::Rook => from.0 == to.0 || from.1 == to.1,
+            PieceType::Rook => from.x == to.x || from.y == to.y,
             PieceType::Bishop => {
-                let dx = (from.0 as i32 - to.0 as i32).abs();
-                let dy = (from.1 as i32 - to.1 as i32).abs();
+                let dx = x_move.abs();
+                let dy = y_move.abs();
                 dx == dy
             }
             PieceType::Knight => {
-                let dx = (from.0 as i32 - to.0 as i32).abs();
-                let dy = (from.1 as i32 - to.1 as i32).abs();
+                let dx = x_move.abs();
+                let dy = y_move.abs();
                 (dx == 2 && dy == 1) || (dx == 1 && dy == 2)
             }
             PieceType::Pawn => {
-                let dx = (from.0 as i32 - to.0 as i32).abs();
-                let dy = (from.1 as i32 - to.1 as i32).abs();
+                let dx = x_move.abs();
+                let dy = y_move.abs();
                 dx == 0 && dy == 1
             }
         }
@@ -117,47 +152,30 @@ impl ChessBoard {
         }
     }
 
-    fn move_piece(&mut self, from: (usize, usize), to: (usize, usize)) {
-        let piece = self.squares[from.0][from.1];
-        self.squares[to.0][to.1] = piece;
-        self.squares[from.0][from.1] = None;
+    fn move_piece(&mut self, from: Coordinate, to: Coordinate) {
+        let piece = self.squares[from.x][from.y];
+        self.squares[to.x][to.y] = piece;
+        self.squares[from.x][from.y] = None;
     }
 
-    fn check_move(&self, from: (usize, usize), to: (usize, usize)) -> bool {
-        let from_piece_opt = self.squares[from.0][from.1];
-        let to_piece = self.squares[to.0][to.1];
+    fn check_move(&self, from: Coordinate, to: Coordinate) -> bool {
+        let from_piece = self.squares[from.x][from.y];
+        let to_piece = self.squares[to.x][to.y];
 
         if from == to {
             return false;
         }
 
-        match from_piece_opt {
-            Some(from_piece) => match to_piece {
-                Some(to_piece) => match from_piece {
-                    ChessPiece::Black(t) => match to_piece {
-                        ChessPiece::Black(_) => {
-                            return false;
-                        }
-                        ChessPiece::White(t) => {
-                            return t.is_move_valid(from, to);
-                        }
-                    },
-                    ChessPiece::White(t) => match to_piece {
-                        ChessPiece::Black(t) => {
-                            return t.is_move_valid(from, to);
-                        }
-                        ChessPiece::White(_) => {
-                            return false;
-                        }
-                    },
-                },
-                None => {
-                    return false;
-                }
-            },
+        let from_piece = match from_piece {
+            Some(from_piece) => from_piece,
             None => {
                 return false;
             }
+        };
+
+        match to_piece {
+            Some(to_piece) => to_piece.can_take(from_piece, from, to),
+            None => true,
         }
     }
 }
@@ -179,43 +197,31 @@ fn take_turn(board: &mut ChessBoard) {
     }
 
     match parse_input(&input) {
-        Some(((fromx, fromy), (tox, toy))) => {
-            if board.check_move((fromx, fromy), (tox, toy)) {
-                board.move_piece((fromx, fromy), (tox, toy));
+        Ok((from, to)) => {
+            if board.check_move(from, to) {
+                board.move_piece(from, to);
             }
         }
-        None => {
+        Err(_) => {
             println!("Invalid input");
             return;
         }
     };
 }
 
-// Will parse the input string into a tuple of two tuples, each representing a coordinate on the board. If the input is invalid, it will return None.
-fn parse_input(input: &str) -> Option<((usize, usize), (usize, usize))> {
+// Will parse the input string into a tuple of two tuples, each representing a coordinate on the board. If the input is invalid, it will return Error.
+fn parse_input(input: &str) -> Result<(Coordinate, Coordinate), ParseIntError> {
     let parts: Vec<&str> = input.split_whitespace().collect();
     if parts.len() != 4 {
-        return None;
+        //TODO: Return some error;
     }
-    let from = match parse_coords(parts[0], parts[1]) {
-        Some(coords) => coords,
-        None => return None,
-    };
-    let to = match parse_coords(parts[2], parts[3]) {
-        Some(coords) => coords,
-        None => return None,
-    };
-    Some((from, to))
+    let from = parse_coords(parts[0], parts[1])?;
+    let to = parse_coords(parts[2], parts[3])?;
+    Ok((from, to))
 }
 
-fn parse_coords(x: &str, y: &str) -> Option<(usize, usize)> {
-    let x = match x.parse::<usize>() {
-        Ok(n) => n,
-        Err(_) => return None,
-    };
-    let y = match y.parse::<usize>() {
-        Ok(n) => n,
-        Err(_) => return None,
-    };
-    Some((x, y))
+fn parse_coords(x: &str, y: &str) -> Result<Coordinate, ParseIntError> {
+    let x = x.parse::<usize>()?;
+    let y = y.parse::<usize>()?;
+    Ok(Coordinate::new(x, y))
 }
